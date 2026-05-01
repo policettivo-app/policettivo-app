47088e6d4134b72852c100d0bc1737f9c7637151
policettivo-app <appuntamentimft@gmail.com>
2026-05-01 14:16:56 +0200

    Add files via upload

---
+++ modifica-test.py
@@ -0,0 +1,3 @@
+content = open('/Users/giulianobaron/Desktop/policettivo-app/paziente.html').read()
+print('section-title trovato:', 'Dati anagrafici' in content)
+print('btn-modifica trovato:', 'btn-modifica' in content)
