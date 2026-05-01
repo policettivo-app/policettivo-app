1b4f1f3c3251bc205622b9a83ffa3375b1034ca9
policettivo-app <appuntamentimft@gmail.com>
2026-05-01 12:33:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'policettivo-v1'
+const FILES = ['/protocollo.html', '/esercizio.html', '/manifest.json']
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
