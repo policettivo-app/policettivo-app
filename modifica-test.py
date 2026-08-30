9b6ac6768ba3543cf34cd2843b52b6cedc099231
Giuliano <appuntamentimft@gmail.com>
2026-08-30 16:48:28 +0200

    pulizia: doppioni e backup vecchi spostati in _archivio (fuori dal repo, conservati su disco e in git history)

--- modifica-test.py
+++
@@ -1,3 +0,0 @@
-content = open('/Users/giulianobaron/Desktop/policettivo-app/paziente.html').read()
-print('section-title trovato:', 'Dati anagrafici' in content)
-print('btn-modifica trovato:', 'btn-modifica' in content)
